a80269a3f6871c22cdeaa0033ed7b20da5892232
Collum Powers <collumpowers@Collums-MBP.attlocal.net>
2023-07-25 20:24:21 -0400

    updated second Excersise

--- excersises/ExcersiseTwo.py
+++ excersises/ExcersiseTwo.py
@@ -1,3 +1,13 @@
-class Pokemon
-    def __init__():
-        self.geek = ""
+class Giraffe():
+    def __init__(self, name, energy):
+        self.name = name
+        self.energy = energy
+        self.height = 79
+    def eat(self):
+        food = input("how much do you want to feed your animal?")
+        self.energy += int(food)
+        print(f"you fed {self.name} {food} pieces of food there energy is currently {self.energy}")
+
+
+Jeff = Giraffe("Jeff", 10)
+Jeff.eat()
